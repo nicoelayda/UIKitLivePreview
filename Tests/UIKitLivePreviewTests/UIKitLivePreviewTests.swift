2e38ab1111e3a42e7bc7a0654ffee4ba1996ea21
Dominic Elayda <nico@elayda.com>
2021-04-28 23:11:24 +0800

    Clear out tests.

--- Tests/UIKitLivePreviewTests/UIKitLivePreviewTests.swift
+++ Tests/UIKitLivePreviewTests/UIKitLivePreviewTests.swift
@@ -2,10 +2,5 @@
     @testable import UIKitLivePreview
 
     final class UIKitLivePreviewTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(UIKitLivePreview().text, "Hello, World!")
-        }
+        static var allTests: [(String, (UIKitLivePreviewTests) -> () -> ())] = []
     }
