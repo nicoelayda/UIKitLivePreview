b583cbc41b2d51e6a8ef653e7079cbc126571c25
Dominic Elayda <nico@elayda.com>
2022-01-22 13:00:20 +0800

    Modify LandscapeModifier to use Xcode 13's new InterfaceOrientation modifiers.

--- Sources/UIKitLivePreview/Utilities/LandscapeModifier.swift
+++ Sources/UIKitLivePreview/Utilities/LandscapeModifier.swift
@@ -9,23 +9,66 @@ import SwiftUI
 @available(iOS 13, *)
 public struct LandscapeModifier: ViewModifier {
     
-    // Reverse the screen width and height so it is displayed in landscape.
-    let height = UIScreen.main.bounds.width
-    let width = UIScreen.main.bounds.height
-    
-    public func body(content: Content) -> some View {
-        content
-            .previewLayout(
-                .fixed(width: width, height: height)
-            )
+    @State private var _orientation: Any? = nil
+    @available(iOS 15, *)
+    private var orientation: LandscapeModifierOrientation {
+        get {
+            _orientation.map {
+                $0 as! LandscapeModifierOrientation
+            } ?? .right
+        }
+        set {
+            _orientation = newValue
+        }
     }
     
+    public init() { }
+    
+    @available(iOS 15, *)
+    public init(orientation: LandscapeModifierOrientation) {
+        self.orientation = orientation
+    }
+    
+    public func body(content: Content) -> some View {
+        if #available(iOS 15, *) {
+            content.previewInterfaceOrientation(orientation.interfaceOrientation)
+        } else {
+            // Reverse the screen width and height so it is displayed in landscape.
+            content
+                .previewLayout(
+                    .fixed(
+                        width: UIScreen.main.bounds.height,
+                        height: UIScreen.main.bounds.width
+                    )
+                )
+        }
+       
+    }
+    
+}
+
+@available(iOS 15, *)
+public enum LandscapeModifierOrientation {
+    case left
+    case right
+    
+    fileprivate var interfaceOrientation: InterfaceOrientation {
+        switch self {
+        case .left: return .landscapeLeft
+        case .right: return .landscapeRight
+        }
+    }
 }
 
 @available(iOS 13, *)
 extension View {
     public func landscape() -> some View {
         modifier(LandscapeModifier())
+    }
+    
+    @available(iOS 15, *)
+    public func landscape(orientation: LandscapeModifierOrientation) -> some View {
+        modifier(LandscapeModifier(orientation: orientation))
     }
 }
 #endif
